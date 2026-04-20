f50a9c824d607a688e1325a33d5c9bca676e9914
Aaron Wolf <aaron.wolf@gmail.com>
2026-04-20 12:35:10 +0300

    api

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
-import pizza from './pizza.js';
+import express from 'express';
 
-console.log('Hello from node!');
+const app = express();
 
-pizza(20, 'pineapple', 'anchovies');
+app.use(express.urlencoded({ extended: true })); // for form data
+app.use(express.json()); // for JSON
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.get('/airplane/', (req, res) => {
+  res.json({
+    model: 'F-35 Lightening II',
+    stealth: true,
+    workingCondition: true
+  });
+});
+
+app.get('/yummy/:food', (req, res) => {
+  console.log('req.params: ', req.params);
+  res.send(`Yes. ${req.params.food} is delicious!`);
+});
+
+app.post('/', (req, res) => {
+  res.send('Post received');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
